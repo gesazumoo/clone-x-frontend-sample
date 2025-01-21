39991d2823b3a4395879e0092b9011a4135e6830
gesazumoo <gesazumoo@huinnoaim.com>
2025-01-21 18:21:19 +0900

    #2 Init LoginPage

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
-  <h2>hello world hello Git</h2>
+  <div class="container">
+    <LoginPage />
+  </div>
 </template>
 
 <script>
+import LoginPage from "./views/LoginPage.vue";
 export default {
   name: "App",
+  components: { LoginPage },
 };
 </script>
 
-<style></style>
+<style>
+body {
+  background-color: black;
+  color: white;
+}
+
+.container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
